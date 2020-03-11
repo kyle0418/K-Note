ac35dc1a0334eeef7e654976eed152598e09c153
Kyle <1396676171@qq.com>
2020-03-11 15:14:31 +0800

    Add files via upload

---
+++ tttest.py
@@ -0,0 +1,11 @@
+print("START")
+temp = input("please input a num:")
+guess = int(temp)
+if guess == 8:
+    print("yesss")
+else:
+    if guess > 8:
+        print("large")
+    else:
+        print("small")
+print("end")
